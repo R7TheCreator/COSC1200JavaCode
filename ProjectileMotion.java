1c3884c1a546eff3fc8361a3bd107100fc3e1942
R7TheCreator <rasulamini27@gmail.com>
2026-03-10 14:17:57 -0400

    start
    just added a menu option with prints and imports nothing too big as I am tired and I will work more on this on later days.

---
+++ ProjectileMotion.java
@@ -0,0 +1,27 @@
+// Name: Rasul Amini
+// Date: March 10th, 2026
+// Description: Assignment for class COSC1200
+
+// setup scanner
+import java.util.Scanner;
+
+// import random
+import java.util.Random;
+
+public class ProjectileMotion {
+    public static void main(String[] args){
+
+        // import scanner
+        Scanner input = new Scanner(System.in);
+
+        // Starting menu
+        System.out.println("Welcome to the projectile game!");
+        // Rules
+        System.out.println("How to play: First select which player you are, " +
+                "and type in your desired angle and power for your shot. " +
+                "First to eliminate the other play wins!");
+        // Selection
+        System.out.println("1. Start");
+        System.out.println("2. Quit");
+    }
+}

3d245906890614951c300ee51b69f49bbd2d01b1
R7TheCreator <rasulamini27@gmail.com>
2026-03-12 12:53:19 -0400

    function & name
    added player names, score, and the getAngle function. will do more functions later today, hopefully get all the functions done by today and stitch them all together by friday.

--- ProjectileMotion.java
+++ ProjectileMotion.java
@@ -11,17 +11,56 @@ import java.util.Random;
 public class ProjectileMotion {
     public static void main(String[] args){
 
+        // Variables
+
+        // player variables
+        String playerOne = " ";
+        String playerTwo = " ";
+        int playerOneScore = 0;
+        int playerTwoScore = 0;
+
+        // loop variable
+        boolean isActive = true;
+
         // import scanner
         Scanner input = new Scanner(System.in);
 
-        // Starting menu
-        System.out.println("Welcome to the projectile game!");
-        // Rules
-        System.out.println("How to play: First select which player you are, " +
-                "and type in your desired angle and power for your shot. " +
-                "First to eliminate the other play wins!");
-        // Selection
-        System.out.println("1. Start");
-        System.out.println("2. Quit");
+        // loop
+        while (isActive){
+            // Starting menu
+            System.out.println("--- Welcome to the projectile game ---");
+            // Rules
+            System.out.println("How to play: First select which player you are, " +
+                    "and type in your desired angle and power for your shot. " +
+                    "First to eliminate the other play wins!");
+
+            System.out.println("-- Select player names --");
+            // player naming
+            System.out.print("Player One Name: ");
+            playerOne = input.nextLine();
+            System.out.print("Player Two Name: ");
+            playerTwo = input.nextLine();
+
+            if (playerOne.equalsIgnoreCase(playerTwo)){
+                System.out.println("Select different names");
+            }
+
+            else System.out.println("Your names are: " + playerOne + " and " + playerTwo);
+
+        }
+    }
+    public static double getAngle(String playerName, Scanner input){
+        boolean isLoop = true;
+        double angle = 0;
+        while (isLoop){
+            System.out.print(playerName + "Enter an Angle: ");
+            angle = input.nextDouble();
+
+            if (angle >= 0 && angle <= 180){
+                isLoop = false;
+            }
+            else System.out.println("Invalid angle");
+        }
+        return angle;
     }
 }
